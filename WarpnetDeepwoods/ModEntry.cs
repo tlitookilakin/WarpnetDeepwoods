4b4d99325c24193e3c968442d021ad3554b3efa8
Wren <Wren@MYSTERY-SHACK>
2021-04-22 20:37:28 -0400

    Add project files.

---
+++ WarpnetDeepwoods/ModEntry.cs
@@ -0,0 +1,61 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarpnetDeepwoods
+{
+    public class ModEntry : Mod
+    {
+        internal static WarpnetDeepwoods.Config Config;
+        private static IWarpNetAPI WarpApi = null;
+        public override void Entry(IModHelper helper)
+        {
+            Config = helper.ReadConfig<Config>();
+            helper.Events.GameLoop.GameLaunched += SetupIntegrations;
+        }
+        private void SetupIntegrations(object sender, GameLaunchedEventArgs ev)
+        {
+            Config.RegisterModConfigMenu(Helper, ModManifest);
+            WarpApi = Helper.ModRegistry.GetApi<IWarpNetAPI>("tlitookilakin.warpnetwork");
+            WarpApi.AddCustomDestinationHandler("deepwoods", WarpToDeepWoods, CanWarpToDeepWoods, GetWarpLabel);
+        }
+        private string GetWarpLabel(string s)
+        {
+            return Helper.Translation.Get("ui-label");
+        }
+        private void WarpToDeepWoods(string s)
+        {
+            Game1.exitActiveMenu();
+            string assemblyname = typeof(DeepWoodsMod.ModEntry).Assembly.FullName;
+            Type type = Type.GetType("DeepWoodsMod.WoodsObeliskMenu, "+assemblyname);
+            Game1.activeClickableMenu = (IClickableMenu)Activator.CreateInstance(type);
+        }
+        private bool CanWarpToDeepWoods(string s)
+        {
+            if (!Config.AfterObelisk)
+            {
+                return true;
+            }
+            Farm farm = Game1.getFarm();
+            if(farm is null)
+            {
+                return false;
+            }
+            foreach(Building building in farm.buildings)
+            {
+                if(building.buildingType.ToString() == "Woods Obelisk")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
